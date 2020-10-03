8414334df075273ba75ae4140fe2572b37b7d774
YoussefMohamed99 <48597215+YoussefMohamed99@users.noreply.github.com>
2020-10-03 21:52:18 +0200

    Adding the filtering methods.
    Added the 4 new methods to filter the products by clothes, shoes, bags, and accessories.

--- lastone/app/Http/Controllers/productcontroller.php
+++ lastone/app/Http/Controllers/productcontroller.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\product;
+use Illuminate\Support\Facades\DB;
+
 class productcontroller extends Controller
 {
     public function index(){
@@ -18,4 +20,29 @@ class productcontroller extends Controller
 
         return view('product')->with('product',$product);
     }
+
+    function getClothes()
+    {
+        $clothes = DB::table('products')->where('category_id', '1')->get();
+        return $clothes;
+    }
+
+    function getShoes()
+    {
+        $shoes = DB::table('products')->where('category_id', '2')->get();
+        return $shoes;
+    }
+
+    function getBags()
+    {
+        $bags = DB::table('products')->where('category_id', '3')->get();
+        return $bags;
+    }
+
+    function getAccessories()
+    {
+        $accessories = DB::table('products')->where('category_id', '4')->get();
+        return $accessories;
+    }
+
 }
